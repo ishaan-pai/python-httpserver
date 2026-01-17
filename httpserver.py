207afd542e3537e31f7b2ae4d73868779891dac2
Ishaan Pai <ishaanpai.dev@gmail.com>
2026-01-17 15:50:13 -0500

    minor changes.

--- httpserver.py
+++ httpserver.py
@@ -5,17 +5,12 @@
 
 import socket
 
-
-# Define socket host and port
-SERVER_HOST = '0.0.0.0'
-SERVER_PORT = 8000
-
 # Create socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.bind(("0.0.0.0", 8000))
 server_socket.listen(1)
-print('Listening on port %s ...' % SERVER_PORT)
+print('Listening on port %s ...' % 8000)
 
 while True:
     # Wait for client connections
